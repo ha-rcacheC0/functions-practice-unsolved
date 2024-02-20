aa2b1fb9a1f541b8bc6c94934ea803cda5219d54
Lucas Keller <148141760+ha-rcacheC0@users.noreply.github.com>
2024-02-20 13:43:55 -0600

    Got my lesson one functions completed using different strategies and am now ready to commit these changes to run the npm tests on my local machine.

--- exercises/e1.js
+++ exercises/e1.js
@@ -15,6 +15,10 @@
 
 // Your code goes here
 
+function logMessage() {
+  console.log('Hello, World!')
+}
+
 
 
 
@@ -31,6 +35,9 @@
 
 // Your code goes here...
 
+function logMyMessage(myMessage) {
+  console.log(myMessage);
+}
 
 
 
@@ -46,7 +53,9 @@
 
 // Your code goes here...
 
-
+function logMessageWithMyName(name) {
+  console.log("My name is " + name + "!");
+}
 
 
 /**
@@ -62,7 +71,9 @@
 
 // Your code goes here...
 
-
+function logMyInfo(name, age) {
+  console.log(`My name is ${name}. I am ${age} years old!`);
+}
 
 
 // === TEST YOURSELF ===
